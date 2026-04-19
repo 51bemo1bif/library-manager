254429f29a2ac8a6fcf9c916b6d0380aba3ab6c2
Mohamed <240074890+51bemo1bif@users.noreply.github.com>
2026-04-19 02:41:50 +0200

    added a new class where it actually manipulate the data of books , which is the only one that have permission to do so

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -1,4 +1,28 @@
 package com.library.repository;
+import com.library.db.DatabaseManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class BookRepository {
+    public void addBook(String title, String author, int year) {
+        // Code to insert a new book into the database
+        String sql = "INSERT INTO books (title, author, year) VALUES (?, ?, ?)";
+        try(Connection connection = DatabaseManager.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+
+            stmt.setString(1, title);
+            stmt.setString(2, author);
+            stmt.setInt(3, year);
+            stmt.executeUpdate();
+            System.out.println("Book added successfully: " + title);
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error adding book: " + e.getMessage());
+        }
+
+    }
 }
